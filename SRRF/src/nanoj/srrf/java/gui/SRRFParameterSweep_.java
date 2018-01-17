60663a1ca0879713b1cf8cb8354dcc35d4a9477e
Siân Culley <s.culley@ucl.ac.uk>
2018-01-17 10:41:39 +0000

    work on gui options.

--- SRRF/src/nanoj/srrf/java/gui/SRRFParameterSweep_.java
+++ SRRF/src/nanoj/srrf/java/gui/SRRFParameterSweep_.java
@@ -7,17 +7,20 @@ import nanoj.srrf.java._BaseSRRFDialog_;
 
 import java.util.Map;
 
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+
 /**
  * Created by sculley on 15/01/2018.
  */
 public class SRRFParameterSweep_ extends _BaseSRRFDialog_ {
 
-    public boolean doDriftCorrection, doIntensityWeighting, radialityPositivityConstraint, doBatch, doGradSmooth, doGradWeight, doIntegrateLagTimes, doRadialitySquaring, renormalize, doTemporalSubtraction, doLinearise, doMinimizePatterning;
+    public boolean doDriftCorrection, doRR, doTRM, doTRA, doTRPPM, doTRAC2, doTRAC4, doIW, doGW, doIntensityWeighting, radialityPositivityConstraint, doBatch, doGradSmooth, doGradWeight, doIntegrateLagTimes, doRadialitySquaring, renormalize, doTemporalSubtraction, doLinearise, doMinimizePatterning;
     boolean _doDriftCorrection, showAdvancedSettings, _showAdvancedSettings = false;
     public int radialityMagnification, SRRForder, frameStart, frameEnd, framesPerTimePoint, symmetryAxes, blockBorderNotConsideringDrift;
     int _frameStart, _frameEnd, _framesPerTimePoint, _blockFrames, _blockSize, _blockPerTimePoint, _nTimePoints, _blockBorderConsideringDrift, maxTemporalBlock, prefSpatialBlock;
 
-    public float ringRadius, psfWidth;
+    public float minRR, maxRR, ringRadius, psfWidth;
 
     public String driftTablePath, batchFolderPath, saveFolderPath, display;
 
@@ -43,28 +46,67 @@ public class SRRFParameterSweep_ extends _BaseSRRFDialog_ {
         gd.addCheckbox("Do_Drift-Correction (with pre-calculated drift-table)", getPrefs("doDriftCorrection", false));
         gd.addSlider("Radiality_Magnification (default: 2, fast -- slow)", 1, 10, getPrefs("radialityMagnification", 2));
 
-        gd.addCheckbox("Sweep ring radius" , true);
+        gd.addCheckbox("Sweep ring radius", getPrefs("doRR", true));
         gd.addNumericField("Ring radius minimum", getPrefs("minRadius", 0.1), 1);
         gd.addNumericField("Ring radius maximum", getPrefs("maxRadius", 1), 1);
+        gd.addSlider("Constant ring radius (if not sweeping)", 0.1f, 3f, getPrefs("ringRadius", 0.5f));
 
-        
+        gd.addMessage("Select temporal analysis options to test");
+        gd.addCheckbox("TRM", getPrefs("doTRM", true));
+        gd.addCheckbox("TRA", getPrefs("doTRA", true));
+        gd.addCheckbox("TRPPM", getPrefs("doTRPPM", true));
+        gd.addCheckbox("TRAC2", getPrefs("doTRAC2", true));
+        gd.addCheckbox("TRAC4", getPrefs("doTRAC4", true));
 
-        //gd.addMessage("~~ Parameters ~~");
-        gd.addSlider("Ring Radius (default: 0.5)", 0.1f, 3f, getPrefs("ringRadius", 0.5f));
-
-        gd.addMessage("-=-= Advanced Settings =-=-\n", headerFont);
-        gd.addCheckbox("Show_Advanced_Settings", false);
-        gd.addNumericField("Max temporal analysis block size (default: 100)", getPrefs("maxTemporalBlock", 100), 0);
-        gd.addNumericField("Preferred spatial analysis block size (0 - auto)", getPrefs("prefSpatialBlock", 0), 0);
-
-        gd.addMessage("-=-= Preview =-=-\n", headerFont);
-        if (log.useDebugChoices()) {
-            gd.addRadioButtonGroup("Display_Mode", new String[]{"Gradient", "Gradient Ring Sum", "Radiality", "Intensity Interp"}, 4, 1, "Radiality");
-        }
-        gd.addCheckbox("Show_Preview", false);
+        gd.addMessage("Weighting options");
+        gd.addCheckbox("Intensity weighting", getPrefs("doIW", true));
+        gd.addCheckbox("Gradient weighting", getPrefs("doGW", true));
+        gd.addSlider("PSF_FWHM (needed in Gradient Weighting)", 1.00f, 5.00f, getPrefs("PSF_Width", 1.35f) * 2.35);
 
         gd.addMessage("Running mode: "+(prefs.get("NJ.kernelMode", false)?"OpenCL Safe Mode (Java Thread Pool)":"Full OpenCL Acceleration"));
+    }
 
-        //gd.addHelp("file:///Users/nils/Documents/Work/Website/my-website/index.html");
+
+    @Override
+    public boolean loadSettings() {
+
+        doDriftCorrection = gd.getNextBoolean();
+
+        radialityMagnification = (int) max(gd.getNextNumber(), 1);
+
+        doRR = gd.getNextBoolean();
+        minRR = (float)  gd.getNextNumber();
+        maxRR = (float)  gd.getNextNumber();
+        ringRadius = (float)  min(max(gd.getNextNumber(), 0.1f), 3f);
+
+        doTRM = gd.getNextBoolean();
+        doTRA = gd.getNextBoolean();
+        doTRPPM = gd.getNextBoolean();
+        doTRAC2 = gd.getNextBoolean();
+        doTRAC4 = gd.getNextBoolean();
+
+
+        doIW = gd.getNextBoolean();
+        doGW = gd.getNextBoolean();
+        psfWidth = (float) min(max(gd.getNextNumber(),1.0f),5.0f) / 2.35f;
+
+        setPrefs("doDriftCorrection", doDriftCorrection);
+        setPrefs("radialityMagnification", radialityMagnification);
+        setPrefs("doRR", doRR);
+        setPrefs("minRR", minRR);
+        setPrefs("maxRR", maxRR);
+        setPrefs("ringRadius", ringRadius);
+        setPrefs("doTRM", doTRM);
+        setPrefs("doTRA", doTRA);
+        setPrefs("doTRPPM", doTRPPM);
+        setPrefs("doTRAC2", doTRAC2);
+        setPrefs("doTRAC4", doTRAC4);
+        setPrefs("doIW", doIW);
+        setPrefs("doGW", doGW);
+        setPrefs("psfWidth", psfWidth);
+
+        prefs.savePreferences();
+
+        return true;
     }
 }
